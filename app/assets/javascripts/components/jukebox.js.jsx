bbc6a3e862d109614542315ffaf97fea5d17e697
Sebastian Jay <sebastianrjay@gmail.com>
2015-10-05 13:51:29 -0700

    Refactor jukebox#render; add separator lines to root.html

--- app/assets/javascripts/components/jukebox.js.jsx
+++ app/assets/javascripts/components/jukebox.js.jsx
@@ -21,16 +21,20 @@ var Jukebox = React.createClass({
     return { tracks: tracks };
   },
 
+  userInstructions: function() {
+    return (<h4>{ "You haven't recorded any tracks yet! To record a track" +
+    ", click the 'Record' button below the keyboard and start playing. When" +
+    " finished, click the 'Stop Recording' button. To play the organ, use " +
+    "the keys between Caps Lock and Return on your keyboard for the white " +
+    "keys, and the keys between Tab and \\ for the black keys. You can also" +
+    " play a key by selecting and pressing it with the mouse pointer." }</h4>)
+  },
+
   render: function() {
     var content;
     var hText = this.props.role === 'user' ? 'Your Tracks:' : 'Featured Tracks:';
     if(this.props.role == 'user' && this.state.tracks.length === 0) {
-      content = <h4>{ "You haven't recorded any tracks yet! To record a track" +
-      ", click the 'Record' button below the keyboard and start playing. When" +
-      " finished, click the 'Stop Recording' button. To play the organ, use " +
-      "the keys between Caps Lock and Return on your keyboard for the white " +
-      "keys, and the keys between Tab and \\ for the black keys. You can also" +
-      " play a key by selecting and pressing it with the mouse pointer." }</h4>
+      content = this.userInstructions();
     } else if (this.props.role == 'featured' && this.state.tracks.length === 0) {
       content = <h4>{ "Loading..." }</h4>
     } else {
